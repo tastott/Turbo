427e2ec9472221db8ae9187d2e9720abadd266c2
Tim Stott <ta.stott@googlemail.com>
2014-10-28 22:40:04 +0000

    ng directive for 16 segment display

--- TurboPi/public/javascripts/app.ts
+++ TurboPi/public/javascripts/app.ts
@@ -1,11 +1,13 @@
 ///<reference path="./typings/angular.d.ts" />
 ///<reference path="./typings/angular-route.d.ts" />
 ///<reference path="./home-controller.ts" />
+///<reference path="./directives.ts" />
 
 module turbo {
     console.log('inside turbo module');
     angular.module('turboApp', ['ngRoute'])
         .controller('HomeController', controllers.HomeController)
+        .directive('segmentDisplay', directives.SegmentDisplay)
         .config(['$routeProvider', ($routeProvider: ng.route.IRouteProvider) =>{
             $routeProvider.when('/home',
                 {
