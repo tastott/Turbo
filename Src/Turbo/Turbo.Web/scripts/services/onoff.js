7dde9a779328859602a146815101c4f380752847
Tim Stott <ta.stott@googlemail.com>
2015-02-02 20:34:33 +0000

    Fixed typo in onoff.js, fullscreen mode

--- Src/Turbo/Turbo.Web/scripts/services/onoff.js
+++ Src/Turbo/Turbo.Web/scripts/services/onoff.js
@@ -1,7 +1,7 @@
 ﻿var onoff = require('onoff');
 
 //if(!process.argv[2]) throw 'No pin number supplied';
-var pin = parseInt(proces.argv[2]);
+var pin = parseInt(process.argv[2]);
 var input = new onoff.Gpio(pin, 'in', 'rising', { debounceTimeout: 50 });
 input.watch(function (err, value) {
     process.send(new Date().getTime());
